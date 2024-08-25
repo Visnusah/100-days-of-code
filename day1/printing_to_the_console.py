071593ef6239b73e66d45f07dd9482eaf2f0d952
kamlesh sah <Sahk0292@gmail.com>
2024-08-25 22:44:24 +0545

    learn: Printing some text in console

--- day1/printing_to_the_console.py
+++ day1/printing_to_the_console.py
@@ -1,2 +1,9 @@
-print("Hello World!" + " I am learning Python!")
-print("Hello I am Visnu")
+print("Hello World!" + " I am learning Pytho!") # Hello World! I am learning Pytho!
+print("Hello I am Visnu") # Hello I am Visnu
+
+print("print('what to print')")  # print('what to print')
+print("print("Hello World!")") # SyntaxError: invalid syntax
+
+
+# NOTE:
+# ( # ) is used to comment a single line
