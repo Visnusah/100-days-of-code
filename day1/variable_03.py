403bf76a17be352687aa83fbe7d0c26ec210e0bb
kamlesh sah <Sahk0292@gmail.com>
2024-08-25 23:14:17 +0545

    Learn: Variables

---
+++ day1/variable_03.py
@@ -0,0 +1,13 @@
+# Variables can be used to store data of different types, and different types can do different things.
+
+#Here is name_one and name_two variables that stores a string and an integer:
+
+# Simple code for understanding the concept of variables
+name_one = "Visnu"
+print(name_one) # Visnu
+name_two = 1243
+print(name_two) # 1243
+
+name = input("What is your name? ")
+print(name) 
+print(len(name))
