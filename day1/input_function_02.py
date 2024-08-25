a1a3586b9def07d751f9574eeb26831a48af87d0
kamlesh sah <Sahk0292@gmail.com>
2024-08-25 23:07:50 +0545

    Learn: input function

---
+++ day1/input_function_02.py
@@ -0,0 +1,11 @@
+# Q: write a program that brings the number of characters in a user name, 
+# you might need to Google search of a function that calculate the length of a steing in Python
+
+# Question:
+input("What is your name? ")
+
+# Step 1 ( Understanding the Basics)
+print(len("Visnu")) # 5
+# here len() is a function that calculates the length of a string in Python
+
+print(len(input("What is your name? ")))
---
+++ day1/printing_to_the_console_01.py
@@ -0,0 +1,20 @@
+print("Hello") # Hello
+print("Hello I am Visnu") # Hello I am Visnu
+
+print("print('what to print')")  # print('what to print')
+print("print("Hello World!")") # SyntaxError: invalid syntax
+
+print("Hello World!" + " I am learning Python!") # Hello World! I am learning Python!
+print("Hello"+"visnu!") # Hellovisnu!
+print("Hello" + " " + "Visnu") # Hello Visnu
+
+Print("Hello") # NameError: name 'Print'
+
+# NOTE:
+# ( # ) is used to comment a single line
+
+
+
+# ERRORS:
+# SyntaxError: invalid syntax --> This error occurs when the syntax is wrong (line 5)
+# NameError: name 'Print' is not defined --> This error occurs when the function is not defined (line 11)
