e7c02cec36236651902c72a998b2d456396e9174
kamlesh sah <Sahk0292@gmail.com>
2024-08-26 23:47:58 +0545

    Learn: switches the vlues of two variables

---
+++ day1/004_.py
@@ -0,0 +1,21 @@
+# Q: WAP that switches the vlues of two variables. The values stored in a and b should be swapped.
+
+# don't change the code below ⬇️
+a = input("a: ")
+b = input("b: ")
+
+# your code goes here: ➷
+
+# First way
+a,b = b,a
+# Second way
+c = a
+a = b
+b = c
+
+# don't change the code below ⬇️
+print("a: " + a)
+print("b: " + b)
+
+# Hint_1: you shuld not have to type any numbers in the code.
+# Hint_2: you may need to use more variable to store the value of a or b.
