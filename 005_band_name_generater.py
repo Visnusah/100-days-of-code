eaf28cb9bb40ede16a6b95fbb63de4aff8dacaac
kamlesh sah <Sahk0292@gmail.com>
2024-08-27 00:58:27 +0545

    learn: Band name generator

---
+++ 005_band_name_generater.py
@@ -0,0 +1,4 @@
+# Q: Create a band name generator. (sipmple code for understanding input, output, variables and \n in python)
+user_name = input("What is your name? \n")
+band_name = input("What is the name of your first pet? \n")
+print("your band name is " + user_name + " " + band_name)
