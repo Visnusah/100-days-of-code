86c53852d21c5455b3e0fac41be344814052e7b0
kamlesh sah <Sahk0292@gmail.com>
2024-08-27 00:29:15 +0545

    Learn: Naming coversion in python

--- day1/003_variable.py
+++ day1/003_variable.py
@@ -11,3 +11,59 @@ print(name_two) # 1243
 name = input("What is your name? ")
 print(name) 
 print(len(name))
+
+
+# variable -> python programming
+# variable -> java programming
+# id function is used to get the memory location of the variable
+# types of variable naming conventions in python programming language are as follows:
+
+#camelCase
+pythonProgramming = "pfython"
+javaProgramming = "java"
+print(id(pythonProgramming))
+print(id(javaProgramming))
+
+#pascalCase
+PythonProgramming = "python"
+JavaProgramming = "java"
+print(id(PythonProgramming))
+print(id(JavaProgramming))
+
+
+#snake_case
+python_programming = "python"
+java_programming = "java"
+print(id(python_programming))
+print(id(java_programming))
+
+#Flatcase
+pythonprogramming = "python"
+javaprogramming = "java"
+print(id(pythonprogramming))
+print(id(javaprogramming))
+
+
+#upperflatecase
+PYTHONPROGRAMMING = "python"
+JAVAPROGRAMMING = "java"
+print(id(PYTHONPROGRAMMING))
+print(id(JAVAPROGRAMMING))
+
+#camelSnakeCase
+python_Programming = "python"
+java_Programming = "java"
+print(id(python_Programming))
+print(id(java_Programming))
+
+#pascalSnakeCase
+Python_Programming = "python"
+Java_Programming = "java"
+print(id(Python_Programming))
+print(id(Java_Programming))
+
+#ScreamingSnakeCase
+PYTHON_PROGRAMMING = "python"
+JAVA_PROGRAMMING = "java"
+print(id(PYTHON_PROGRAMMING))
+print(id(JAVA_PROGRAMMING))
