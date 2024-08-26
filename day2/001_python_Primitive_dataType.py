82fe2dbf2a7f974767f964e042c28341baa493e8
kamlesh sah <Sahk0292@gmail.com>
2024-08-27 01:10:57 +0545

    Learn: datatypes

---
+++ day2/001_python_Primitive_dataType.py
@@ -0,0 +1,30 @@
+# Data Types
+
+# String
+"Hello" # Hello
+"Hello"[0] # H
+"121" + "868" # 121868
+
+# TO check the type of data
+print(type("Hello")) # <class 'str'>
+print(type("Hello"[2])) # <class 'str'>
+print(type("121" + "868")) # <class 'str'>
+
+
+
+# Integer
+123
+print(type(123)) # <class 'int'>
+# Float
+12.3
+print(type(12.3)) # <class 'float'>
+# Boolean
+True
+False
+print(type(True)) # <class 'bool'>
+print(type(False)) # <class 'bool'>
+# None
+None
+print(type(None)) # <class 'NoneType'>
+a = print("Hello")
+print(a) # <class 'NoneType'>
