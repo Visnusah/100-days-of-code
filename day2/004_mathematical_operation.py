7f3da9a278c0c0568618d89cfb3ed68d4a99ccfa
kamlesh sah <Sahk0292@gmail.com>
2024-08-27 22:09:58 +0545

    learn: mathemarical operation

---
+++ day2/004_mathematical_operation.py
@@ -0,0 +1,19 @@
+# Here is mathematical operation in python
+
+print(3+3) # 6
+print(3-3) # 0
+print(3*3) # 9
+print(3/3) # 1.0 # Division always return float
+print(2**3) # 8 # Exponentiation
+
+# PAMDASLR Rule for mathematical operation in python
+
+# Parentheses ()
+# Exponents **
+# Multiplication and Division * /
+# Addition and Subtraction + -
+# Left to right
+
+# Example
+print(3*3+3/3-3) # 7.0
+print(4 * 3 + 4 / 2 - 3) # 12 + 2 - 3 = 11 --> 11.0
