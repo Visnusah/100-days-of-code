b103f9474f632fd7eb4803d5a16dd8a9e9343bfb
kamlesh sah <Sahk0292@gmail.com>
2024-08-30 20:40:31 +0545

    control flow basics

---
+++ day3/001_control_flow.py
@@ -0,0 +1,9 @@
+# QUE: WAP to check if the user is eligible to ride the rollercoaster based on their height.
+
+print("Wlcome to the rollercoaster!")
+height = int(input("Enter your hieght in cm: "))
+
+if height > 120:
+  print("You can ride the rollercoaster!")
+else:
+  print("Sorry, you have to grow taller before you can ride.")
