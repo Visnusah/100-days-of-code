8fd2438ef2de7f283fb6fe9176a440298dc8c031
kamlesh sah <Sahk0292@gmail.com>
2024-08-30 20:50:06 +0545

    nested if concepts basics

--- day3/001_control_flow.py
+++ day3/001_control_flow.py
@@ -1,9 +1,16 @@
 # QUE: WAP to check if the user is eligible to ride the rollercoaster based on their height.
 
 print("Wlcome to the rollercoaster!")
+age = int(input("enter your age: "))
 height = int(input("Enter your hieght in cm: "))
 
 if height > 120:
   print("You can ride the rollercoaster!")
+  if age <= 12:
+    print("please pay bill of $5.")
+  elif age <= 18:
+    print("please pay bill of $7.")
+  else:
+    print("please pay bill of $12.")
 else:
   print("Sorry, you have to grow taller before you can ride.")
