ba5ce961a4f7bc895d197b3fdda6b4e2448b8b61
kamlesh sah <Sahk0292@gmail.com>
2024-08-31 18:54:17 +0545

    comments the print function because not much needed

--- day3/005_multiple_if.py
+++ day3/005_multiple_if.py
@@ -6,13 +6,13 @@ if height > 120:
   print("You can ride the rollercoaster!")
   if age <= 12:
     bill = 5
-    print("child tickets are $5.")
+    # print("child tickets are $5.")
   elif age <= 18:
     bill = 7
-    print("youth tickets are $7.")
+    # print("youth tickets are $7.")
   else:
     bill = 12
-    print("adult tickets are $12.")
+    # print("adult tickets are $12.")
   wants_photo = input("Do you want a photo taken? Y or N: ")
   if wants_photo == "Y" or wants_photo == "y": # or is used to check multiple conditions
     bill += 3
