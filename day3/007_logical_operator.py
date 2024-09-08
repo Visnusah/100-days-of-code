34d30c261833486b2578f3cc168a8e2d4429bec1
kamlesh sah <Sahk0292@gmail.com>
2024-09-08 21:29:28 +0545

    Logical operator

---
+++ day3/007_logical_operator.py
@@ -0,0 +1,23 @@
+# Example of logical operator
+# Logical operators are used to combine conditional statements
+
+# Example of logical operators
+# Logical operators are used to combine conditional statements
+
+# Example 1: and operator
+a = 5
+b = 3
+
+if a > 0 and b > 0:
+    print("Both a and b are positive")
+
+# Example 2: or operator
+a = 5
+b = -3
+if a > 0 or b > 0:
+    print("At least one of a and b is positive")
+
+# Example 3: not operator
+a = 5
+if not a > 0:
+    print("a is not positive")
